6bddee9c50b18114917d3ab0b35c7bc2f6a6b28a
CarlosRFS <carlosrfs99@gmail.com>
2019-10-06 04:47:48 -0300

    Limpeza do codigo

--- fast_code.sh
+++ fast_code.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#OBSERVAÇÔES
+#==> Adicionar mensagens de erro para diretorios invalidos e arquivos fonte nao encontrados
+#==> Adicionar função de passar como parametro o nome do arquivo se estiver no mesmo diretorio
+#==> Possibilidade de selecionar quais linhas do codigo devem ser enviadas
+#OPCIONAL==> Exibir o link para baixar o fonte do xclip se ele não estiver disponivel no sistema
+#==> Adicionar um menu para utilizar o script mais de uma vez
+#==> Adicionar parametro para habilitar/desabilitar o menu
+
 #cores
 azul="\033[36;1m"
 vermelho="\033[31;1m"
@@ -12,10 +20,13 @@ which xclip >> saida.txt \
 rm saida.txt
 #=======================================================>
 read -p "Digite o diretorio onde se encontra o fonte: " dir
-cd $dir
+cd $dir || $( echo -e "$vermelho Diretorio não encontrado!$fim" && exit; )
 
 read -p "Digite o nome do fonte: " fonte
+#testando se o arquivo existe
+cat $fonte || $( echo -e "$vermelho Arquivo não encontrado!$fim" && exit; )
 
 cat $fonte | netcat termbin.com 9999 >> fonte.txt
 xclip -selection "clipboard" fonte.txt \
 && echo -e "$azul O link para seu codigo fonte ja se encontra no clipboard, basta colar aonde quiser!$fim"
+rm fonte.txt
