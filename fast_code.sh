aad9fefc0ea0f49e6200d8423636d676a2a8881a
CarlosRFS <carlosrfs99@gmail.com>
2019-10-06 04:28:27 -0300

    adicionado cor para destacar algumas mensagens

--- fast_code.sh
+++ fast_code.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+#cores
+azul="\033[36;1m"
+vermelho="\033[31;1m"
+verde="\033[34;1m"
+fim="\033[m"
 
 #Testando se o xclip esta presente no sistema
 which xclip >> saida.txt \
-&& echo "Dependencias OK!" || $(echo "Dependencia xclip não encontrada!" && exit;)
-
+&& echo -e "$verde Dependencias $azul OK!$fim" || $(echo "$vermelho Dependencia xclip não encontrada!$fim" && exit;)
+rm saida.txt
 #=======================================================>
 read -p "Digite o diretorio onde se encontra o fonte: " dir
 cd $dir
@@ -13,4 +18,4 @@ read -p "Digite o nome do fonte: " fonte
 
 cat $fonte | netcat termbin.com 9999 >> fonte.txt
 xclip -selection "clipboard" fonte.txt \
-&& echo "O link para seu codigo fonte ja se encontra no clipboard, basta colar aonde quiser!"
+&& echo -e "$azul O link para seu codigo fonte ja se encontra no clipboard, basta colar aonde quiser!$fim"
