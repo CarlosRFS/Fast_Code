ec76cbed5384a9252f1da884ce5b45fd1bc5da61
CarlosRFS <carlosrfs99@gmail.com>
2019-10-08 22:33:28 -0300

    Refeito

--- fast_code.sh
+++ fast_code.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
 
-#AUTOR ==> Carlos Raimundo de Freitas Sotero
-#Email ==> carlosrfs99@gmail.com
-
-
-#OBSERVAÇÔES
-#==> Possibilidade de selecionar quais linhas do codigo devem ser enviadas
-# talvez usar o grep ou cat e salvar as linhas selecionadas num arquivo temporario
-#OPCIONAL==> Exibir o link para baixar o fonte do xclip se ele não estiver disponivel no sistema
-#==> Adicionar um menu para utilizar o script mais de uma vez
-#==> Adicionar parametro para habilitar/desabilitar o menu
-
 #cores
-azul="\033[36;1m"
+azul="\033[34;1m"
 vermelho="\033[31;1m"
-verde="\033[34;1m"
+verde="\033[36;1m"
 fim="\033[m"
 
-#Testando se o xclip esta presente no sistema
-type xclip &>/dev/null && echo -e "$verde Dependencias $azul OK!$fim" || $( echo -e "$vermelho Dependencia xclip não encontrada!$fim" && exit )
+#Testando as dependencias
+type "xclip" &>/dev/null \
+&& echo -e "$azul Dependencias$verde OK! $fim" \
+|| $( echo -e "$vermelho Dependencia xclip não encontrada! $fim" && exit )
 
-#=======================================================>
-if [[ $1 == 0 ]]; then
-	fonte=$1
+
+#função 
+function link_clipboard(){
+	cat $fonte | netcat termbin.com 9999 >> link.txt
+	xclip -selection "clipboard" link.txt
+}
+
+
+#verificar os parametros posicionais
+if [[ $1 ]]; then
+	if [[ $2 ]]; then
+		a=$3-$2
+		head -$3 $1 | tail -$a >> fonte.txt
+		fonte="fonte.txt"
+	else
+		fonte=$1
+	fi
+	link_clipboard
 else
-	read -p "Digite o diretorio onde se encontra o fonte: " dir
-	cd $dir || $( echo -e "$vermelho Diretorio não encontrado!$fim" && exit; )
-	read -p "Digite o nome do fonte: " fonte
+	echo -e "$azul Digite o caminho para o arquivo do codigo fonte! $fim"
+	read fonte
+	link_clipboard
 fi
-
-#testando se o arquivo existe
-cat $fonte >> /dev/null || $( echo -e "$vermelho Arquivo não encontrado!$fim" && exit; )
-
-cat $fonte | netcat termbin.com 9999 >> fonte.txt
-xclip -selection "clipboard" fonte.txt \
-&& echo -e "$azul O link para seu codigo fonte ja se encontra no clipboard, basta colar aonde quiser!$fim"
-rm fonte.txt
